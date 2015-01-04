85b10824dbab50a1dfbebd942ff0e18f4c8b2c5c
Adam Nicholson <adamnicholson10@gmail.com>
2015-01-04 13:23:15 +0000

    Added basic CommandBus and CommandHandler interface methods

--- src/CommandBus.php
+++ src/CommandBus.php
@@ -4,5 +4,29 @@ namespace Chief;
 
 interface CommandBus
 {
+    /**
+     * Execute a command
+     *
+     * @param Command $command
+     * @return mixed
+     */
+    public function execute(Command $command);
 
+    /**
+     * Map a command to a handler by name
+     *
+     * @param $commandName
+     * @param $handlerName
+     * @return mixed
+     */
+    public function mapHandler($commandName, $handlerName);
+
+    /**
+     * Map a command to a callable handler
+     *
+     * @param $commandName
+     * @param callable $handler
+     * @return mixed
+     */
+    public function pushHandler($commandName, callable $handler);
 }
--- src/CommandHandler.php
+++ src/CommandHandler.php
@@ -4,5 +4,11 @@ namespace Chief;
 
 interface CommandHandler
 {
-
+    /**
+     * Handle a command execution
+     *
+     * @param Command $command
+     * @return mixed
+     */
+    public function handle(Command $command);
 }
