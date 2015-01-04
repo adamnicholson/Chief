f549380b1d5134923564b0089ab5b696aa808e55
Adam Nicholson <adamnicholson10@gmail.com>
2015-01-04 13:36:39 +0000

    Implemented Chief::pushHandler()

--- src/Chief.php
+++ src/Chief.php
@@ -4,5 +4,56 @@ namespace Chief;
 
 class Chief implements CommandBus
 {
+    protected $handlers = [];
+
+    /**
+     * Execute a command
+     *
+     * @param Command $command
+     * @return mixed
+     */
+    public function execute(Command $command)
+    {
+        $handler = $this->findHandler($command);
+
+        if (is_callable($handler)) {
+            $handler($command);
+        }
+    }
+
+    /**
+     * Map a command to a handler by name
+     *
+     * @param $commandName
+     * @param $handlerName
+     * @return mixed
+     */
+    public function mapHandler($commandName, $handlerName)
+    {
+        // TODO: Implement mapHandler() method.
+    }
+
+    /**
+     * Map a command to a callable handler
+     *
+     * @param $commandName
+     * @param callable $handler
+     * @return mixed
+     */
+    public function pushHandler($commandName, callable $handler)
+    {
+        $this->handlers[$commandName] = $handler;
+    }
+
+    protected function findHandler(Command $command)
+    {
+        foreach ($this->handlers as $handlerCommand => $handler) {
+            if ($handlerCommand == get_class($command)) {
+                return $handler;
+            }
+        }
+
+        return null;
+    }
 
 }
--- tests/ChiefTest.php
+++ tests/ChiefTest.php
@@ -8,4 +8,19 @@ class ChiefTest extends ChiefTestCase
     {
         $this->assertTrue(new Chief instanceof CommandBus);
     }
+
+    public function testExecuteFiresHandlerAttachedByPushHandler()
+    {
+        $foo = new \stdClass();
+        $foo->called = false;
+        $handler = function (Command $command) use ($foo) {
+            $foo->called = true;
+        };
+        $chief = new Chief();
+        $chief->pushHandler('Chief\ChiefTestCommandStub', $handler);
+        $chief->execute(new ChiefTestCommandStub);
+        $this->assertEquals($foo->called, true);
+    }
 }
+
+class ChiefTestCommandStub implements Command {}
