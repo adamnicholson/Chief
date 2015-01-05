edbaec5db29620b86d0b991cc564c60bab8fab0a
Adam Nicholson <adamnicholson10@gmail.com>
2015-01-05 17:00:16 +0000

    Added an EventDispatcher implementation for Laravel/Illuminate

---
+++ src/Decorators/Laravel/IlluminateEventDispatcher.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Chief\Decorators\Laravel;
+
+use Chief\Decorators\EventDispatcher;
+use Illuminate\Events\Dispatcher;
+
+class IlluminateEventDispatcher implements EventDispatcher
+{
+    public function __construct(Dispatcher $dispatcher)
+    {
+        $this->dispatcher = $dispatcher;
+    }
+
+    /**
+     * @param string $event The event name
+     * @param array $data The event data
+     * @return void
+     */
+    public function dispatch($event, $data = [])
+    {
+        $this->dispatcher->fire($event, $data);
+    }
+}
--- src/Providers/LaravelServiceProvider.php
+++ src/Providers/LaravelServiceProvider.php
@@ -11,12 +11,29 @@ class LaravelServiceProvider extends ServiceProvider
 {
     public function register()
     {
-        $this->app->bind('Chief\Container', 'Chief\Containers\IlluminateContainer');
+        $this->registerContainer();
 
+        $this->registerCommandBus();
+
+        $this->registerEventDispatcher();
+    }
+
+    protected function registerContainer()
+    {
+        $this->app->bind('Chief\Container', 'Chief\Containers\IlluminateContainer');
+    }
+
+    protected function registerCommandBus()
+    {
         $this->app->bind('Chief\CommandBus', function () {
             $resolver = new NativeCommandHandlerResolver($this->app->make('Chief\Container'));
             $defaultBus = new SynchronousCommandBus($resolver);
             return new Chief($defaultBus);
         });
+    }
+
+    protected function registerEventDispatcher()
+    {
+        $this->app->bind('Chief\Decorators\EventDispatcher', 'Chief\Decorators\Laravel\IlluminateEventDispatcher');
     }
 }
