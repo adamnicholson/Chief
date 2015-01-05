a1a1e99d405f119b1263d9ccab86c73144fc727a
Adam Nicholson <adamnicholson10@gmail.com>
2015-01-05 00:12:20 +0000

    Added unit tests for NativeCommandHandlerResolver

--- tests/Resolvers/NativeCommandHandlerResolverTest.php
+++ tests/Resolvers/NativeCommandHandlerResolverTest.php
@@ -3,6 +3,8 @@
 namespace Chief\Resolvers;
 
 use Chief\ChiefTestCase;
+use Chief\Command;
+use Chief\CommandHandler;
 use Chief\CommandHandlerResolver;
 use Chief\Stubs\TestCommand;
 use Chief\Stubs\TestCommandHandler;
@@ -27,5 +29,38 @@ class NativeCommandHandlerResolverTest extends ChiefTestCase
         $resolver = new NativeCommandHandlerResolver;
         $handler = $resolver->resolve(new TestCommand);
         $this->assertTrue($handler instanceof TestCommandHandler);
+    }
+
+    public function testResolveReturnsHandlerBoundByObject()
+    {
+        $handler = $this->getMock('Chief\CommandHandler');
+        $resolver = new NativeCommandHandlerResolver;
+        $resolver->bindHandler('Chief\Stubs\TestCommandWithoutHandler', $handler);
+        $this->assertEquals($resolver->resolve(new TestCommandWithoutHandler), $handler);
+    }
+
+    public function testResolveReturnsHandlerBoundByCallable()
+    {
+        $resolver = new NativeCommandHandlerResolver;
+        $proof = new \stdClass();
+        $resolver->bindHandler('Chief\Stubs\TestCommandWithoutHandler', function (Command $command) use ($proof) {
+                $proof->handled = true;
+        });
+        $command = new TestCommandWithoutHandler;
+        $handler = $resolver->resolve($command);
+        $this->assertTrue($handler instanceof CommandHandler);
+        $handler->handle($command);
+        $this->assertEquals($proof->handled, true);
+    }
+
+    public function testResolveReturnsHandlerBoundByString()
+    {
+        $resolver = new NativeCommandHandlerResolver;
+        $resolver->bindHandler('Chief\Stubs\TestCommandWithoutHandler', 'Chief\Stubs\TestCommandHandler');
+        $command = new TestCommand;
+        $handler = $resolver->resolve($command);
+        $this->assertTrue($handler instanceof CommandHandler);
+        $handler->handle($command);
+        $this->assertEquals($command->handled, true);
     }
 }
