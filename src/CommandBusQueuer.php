63200f45088b4138fff6f5ea07559e7d6cc07f6c
Adam Nicholson <adamnicholson10@gmail.com>
2015-01-06 18:34:34 +0000

    Fixed docblock

--- src/CommandBusQueuer.php
+++ src/CommandBusQueuer.php
@@ -8,7 +8,6 @@ interface CommandBusQueuer
      * Queue a Command for executing
      *
      * @param Command $command
-     * @return mixed
      */
     public function queue(Command $command);
 }
