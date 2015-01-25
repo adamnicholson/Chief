4216573314de6d9806b0ed7e1b5f712c55e4b229
Adam Nicholson <adamnicholson10@gmail.com>
2015-01-25 15:06:18 +0000

    Removed unused Use statement

--- src/Resolvers/NativeCommandHandlerResolver.php
+++ src/Resolvers/NativeCommandHandlerResolver.php
@@ -9,7 +9,6 @@ use Chief\Container;
 use Chief\Exceptions\UnresolvableCommandHandlerException;
 use Chief\Handlers\CallableCommandHandler;
 use Chief\Handlers\LazyLoadingCommandHandler;
-use Chief\Handlers\StringCommandHandler;
 use Chief\Containers\NativeContainer;
 
 class NativeCommandHandlerResolver implements CommandHandlerResolver
